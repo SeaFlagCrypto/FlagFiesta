f00b3f7106705c39c20e222e7358d1dca322a1a1
SeaFlagCrypto <editorservice4all@gmail.com>
2025-06-18 12:34:15 -0400

    Serve farcaster.json via API route and apply JSON headers using Vercel rewrite

--- api/farcaster.js
+++ api/farcaster.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({
     frame: {
       version: "1",
