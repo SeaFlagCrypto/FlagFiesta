4b06af974b8fb33d9620d16fbafc712739ef9db2
SeaFlagCrypto <editorservice4all@gmail.com>
2025-06-19 17:58:40 -0400

    Add .well-known/farcaster.json for Farcaster Mini App manifest

--- api/farcaster.js
+++ api/farcaster.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "no-store");
+
   res.status(200).json({
     type: "miniapp",
     url: "https://flag-fiesta.vercel.app",
